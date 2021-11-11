afbdd1163f7aff92809f96951e3ab1b42bd31cf6
MohamedMedhat96 <mohamed.medhat-ali@student.guc.edu.eg>
2021-11-11 04:37:33 +0200

    Adding port

--- config/initializers/ElasticSearchIndex.rb
+++ config/initializers/ElasticSearchIndex.rb
@@ -1,4 +1,3 @@
-puts ENV['ELASTICSEARCH_URL']
 Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['ELASTICSEARCH_URL'])
 unless Message.__elasticsearch__.index_exists?
   puts "HEREEE"

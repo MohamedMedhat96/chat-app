e9df4b47bd4d0240c7d03d44ff70e051cf5b03a7
MohamedMedhat96 <mohamed.medhat-ali@student.guc.edu.eg>
2021-11-20 18:56:32 +0200

    Fixing schema and modiyfing dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@ RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs &&
 RUN apt-get install bash
 RUN mkdir /app
 WORKDIR /app 
-ADD . /app
 ADD Gemfile /Gemfile
 ADD Gemfile.lock /Gemfile.lock
 RUN gem install bundler -v 2.2.31
 ENV BUNDLER_VERSION 2.2.31
 RUN bundle install
 RUN bundle clean --force
+ADD . /app
 
 COPY bin/wait-for-it.sh /app/bin/wait-for-it.sh
 RUN dos2unix bin/docker-start
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,5 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_09_140451) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string "token"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "chats_count", default: 0
+    t.index ["name"], name: "index_applications_on_name", unique: true
+    t.index ["token"], name: "index_applications_on_token", unique: true
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "application_id"
+    t.integer "messages_count", default: 0
+    t.index ["application_id", "number"], name: "index_chats_on_application_id_and_number", unique: true
+    t.index ["application_id"], name: "index_chats_on_application_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "number"
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "chat_id"
+    t.index ["chat_id", "number"], name: "index_messages_on_chat_id_and_number", unique: true
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
+
 end
